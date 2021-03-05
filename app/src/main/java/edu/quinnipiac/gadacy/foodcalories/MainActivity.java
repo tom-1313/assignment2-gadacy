05aeaa804782b991a42e25ed1b6316ae21c1a220
tom-1313 <trgadacy@quinnipiac.edu>
2021-03-05 18:26:47 -0500

    first commit

---
+++ app/src/main/java/edu/quinnipiac/gadacy/foodcalories/MainActivity.java
@@ -0,0 +1,28 @@
+package edu.quinnipiac.gadacy.foodcalories;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
+/*
+TODO: 1. Rest API Working
+        a. Update Spinner with text from API
+        b. Use JSON from Videos
+        c. Use AysnTask
+      2. Create Toolbar
+        a. Share option
+        b. Setting Option
+            i. Allow user to change background colors
+        c. Help option
+            i. Display info about API and developer
+            
+ */
